953b3686ee8086058fd403285865a0fe29e9118b
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-10-02 00:19:31 +0530

    ui updates

--- src/components/Note.js
+++ src/components/Note.js
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 
 const Note = ({ id, title, description, file_path }) => {
   return (
-    <div className="relative">
-      <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 border-solid border-2 border-black hover:border-lime-300 m-2">
-        <div className="p-6">
+    <div className="relative ">
+      <div className="relative group peer flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 border-solid border-2 border-black hover:border-lime-300 m-2">
+        <div className="p-4">
           <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
             {title}
           </h5>
@@ -15,7 +15,7 @@ const Note = ({ id, title, description, file_path }) => {
             {description}
           </p>
         </div>
-        <div className="p-6 pt-0">
+        <div className="p-4 pt-0">
           {file_path ? (
             <a href={file_path}>
               <button
@@ -35,26 +35,26 @@ const Note = ({ id, title, description, file_path }) => {
             </button>
           )}
           {/* button to update */}
-          
-          
-        </div>
-      </div>
-        <button
-            className="absolute top-0 right-0   select-none rounded-lg bg-red-600 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
-            data-ripple-light="true"
-          >
-            <MdOutlineDelete size={20} />
-          </button>
-          <Link to={`/${id}`}>
+          <Link to={`/${id}`} className="invisible group-hover:visible">
             <button
-              className=" absolute bottom-0 right-0 select-none rounded-lg bg-green-300 py-2 px-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              className="absolute right-2 select-none rounded-lg bg-green-300 py-2 px-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               type="button"
               data-ripple-light="true"
             >
               <PiPencilSimpleBold size={20} />
             </button>
           </Link>
+          <button
+            className="absolute group-hover:visible invisible right-2 top-2  select-none rounded-lg bg-red-600 py-2 px-3 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+            data-ripple-light="true"
+          >
+            <MdOutlineDelete size={20} />
+          </button>
+          
+        </div>
+      </div>
+        
     </div>
   );
 };
